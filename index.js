eb926d865e4080d72b17114544d91c542344619a
Ismail El Korchi <ismail.elkorchi@gmail.com>
2022-10-05 00:48:49 +0100

    Refactor the main function

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 function parseFlag ( targetArgument, argumentType ) {
 	const processArguments = process.argv;
-	let argsIndex = [];
 
-	for ( const key in processArguments ) {
-		if ( processArguments[key].charAt( 0 ) === '-' && processArguments[key].charAt( 0 ) === '-' ) {
-			argsIndex.push( [ processArguments[key], key ] );
+	if ( processArguments.includes( targetArgument ) ) {
+
+		if ( argumentType === 'string' ) {
+			return processArguments[processArguments.indexOf( targetArgument ) + 1];
 		}
-	}
+	
+		if ( argumentType === 'array' ) {
+	
+			let flagsIndex = [];
 
-	for ( const key in processArguments ) {
-		if ( argumentType === 'string' && processArguments[key] === targetArgument ) {
-			return processArguments[Number( key ) + 1];
-		}
+			for ( const key in processArguments ) {
+				if ( processArguments[key].charAt( 0 ) === '-' && processArguments[key].charAt( 0 ) === '-' ) {
+					flagsIndex.push( [ processArguments[key], key ] );
+				}
+			}
 
-		if ( argumentType === 'array' && processArguments[key] === targetArgument ) {
-			for ( const key in argsIndex ) {
-				if ( argsIndex[key][0] === targetArgument ) {
-					if ( argsIndex[key][1] > argsIndex[argsIndex.length - 1][1] ) {
-						return processArguments.slice( Number( argsIndex[key][1] ) + 1, argsIndex[Number( key ) + 1][1] );
+			for ( const key in flagsIndex ) {
+				if ( flagsIndex[key][0] === targetArgument ) {
+					if ( flagsIndex[key][1] > flagsIndex[flagsIndex.length - 1][1] ) {
+						return processArguments.slice( Number( flagsIndex[key][1] ) + 1, flagsIndex[Number( key ) + 1][1] );
 					} else {
-						return processArguments.slice( Number( argsIndex[key][1] ) + 1 );
+						return processArguments.slice( Number( flagsIndex[key][1] ) + 1 );
 					}
 				}
 			}
 		}
-
+	
 		if ( argumentType === 'boolean' ) {
-			if ( processArguments.indexOf( targetArgument ) !== -1 ) {
-				return true;
-			}
-			return false;
+			return true;
 		}
-
-		if ( argumentType === 'number' && processArguments[key] === targetArgument ) {
-			return Number( processArguments[Number( key ) + 1] );
+	
+		if ( argumentType === 'number' ) {
+			return Number( processArguments[processArguments.indexOf( targetArgument ) + 1] );
 		}
 	}
+
+	return false;
 }
 
 export { parseFlag };
